bf930bf01035a5230415c8ef7d2b579d23cf852a
B-spoked <jamie@almservices.co.nz>
2012-12-14 23:01:26 +1300

    use responsive, table for log data

--- js/betes.js
+++ js/betes.js
@@ -87,7 +87,9 @@ $( function( $ ) {
 	app.LogBookEntryView = Backbone.View.extend({
 
 		//... is a list tag.
-		tagName:  'li',
+		tagName:  'tr',
+		
+		className: 'warning',
 
 		// Cache the template function for a single item.
 		template: _.template( $('#item-template').html() ),

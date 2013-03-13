fb4a9fd0063ed6fe5853846fb25043d6d24e3afc
B-spoked <jamie@almservices.co.nz>
2013-03-14 07:42:39 +1300

    start integration with mongodb

--- rest_api/routes/logbook.js
+++ rest_api/routes/logbook.js
@@ -1,41 +1,87 @@
-exports.findAll = function(req, res) {
-	res.send([ {
-		name : 'user1'
-	}, {
-		name : 'user2'
-	}, {
-		name : 'user3'
-	} ]);
-};
+var mongo = require('mongodb');
+
+var Server = mongo.Server,
+    Db = mongo.Db,
+    BSON = mongo.BSONPure;
+
+var server = new Server('localhost', 27017, {auto_reconnect: true});
+db = new Db('winedb', server, {safe: true});
+
+db.open(function(err, db) {
+    if(!err) {
+        console.log("Connected to 'winedb' database");
+        db.collection('wines', {safe:true}, function(err, collection) {
+            if (err) {
+                console.log("The 'wines' collection doesn't exist. Creating it with sample data...");
+                populateDB();
+            }
+        });
+    }
+});
 
 exports.findById = function(req, res) {
-	res.send({
-		id : req.params.id,
-		name : "The Name",
-		description : "description"
-	});
+    var id = req.params.id;
+    console.log('Retrieving wine: ' + id);
+    db.collection('wines', function(err, collection) {
+        collection.findOne({'_id':new BSON.ObjectID(id)}, function(err, item) {
+            res.send(item);
+        });
+    });
 };
 
-exports.addResult = function(req, res) {
-	res.send({
-		id : req.params.id,
-		name : "The Name",
-		description : "description"
-	});
+exports.findAll = function(req, res) {
+    db.collection('wines', function(err, collection) {
+        collection.find().toArray(function(err, items) {
+            res.send(items);
+        });
+    });
 };
 
-exports.updateResult = function(req, res) {
-	res.send({
-		id : req.params.id,
-		name : "The Name",
-		description : "description"
-	});
-};
+exports.addWine = function(req, res) {
+    var wine = req.body;
+    console.log('Adding wine: ' + JSON.stringify(wine));
+    db.collection('wines', function(err, collection) {
+        collection.insert(wine, {safe:true}, function(err, result) {
+            if (err) {
+                res.send({'error':'An error has occurred'});
+            } else {
+                console.log('Success: ' + JSON.stringify(result[0]));
+                res.send(result[0]);
+            }
+        });
+    });
+}
 
-exports.deleteResult = function(req, res) {
-	res.send({
-		id : req.params.id,
-		name : "The Name",
-		description : "description"
-	});
-};
+exports.updateWine = function(req, res) {
+    var id = req.params.id;
+    var wine = req.body;
+    delete wine._id;
+    console.log('Updating wine: ' + id);
+    console.log(JSON.stringify(wine));
+    db.collection('wines', function(err, collection) {
+        collection.update({'_id':new BSON.ObjectID(id)}, wine, {safe:true}, function(err, result) {
+            if (err) {
+                console.log('Error updating wine: ' + err);
+                res.send({'error':'An error has occurred'});
+            } else {
+                console.log('' + result + ' document(s) updated');
+                res.send(wine);
+            }
+        });
+    });
+}
+
+exports.deleteWine = function(req, res) {
+    var id = req.params.id;
+    console.log('Deleting wine: ' + id);
+    db.collection('wines', function(err, collection) {
+        collection.remove({'_id':new BSON.ObjectID(id)}, {safe:true}, function(err, result) {
+            if (err) {
+                res.send({'error':'An error has occurred - ' + err});
+            } else {
+                console.log('' + result + ' document(s) deleted');
+                res.send(req.body);
+            }
+        });
+    });
+}

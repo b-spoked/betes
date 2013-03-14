2f100a508e5d25792f55341d989a8ed53e00e7ae
B-spoked <jamie@almservices.co.nz>
2013-03-14 22:18:41 +1300

    add needed images

--- web_app/api/config.php
+++ web_app/api/config.php
@@ -1,4 +1,4 @@
 <?php
-define('DB_SERVER', 'mysql:host=beteslogDb.db.7946997.hostedresource.com;dbname=heroku_fee62f08e9a254b');
+define('DB_SERVER', 'mysql://b95d456714e818:e9ec1df8@us-cdbr-east-03.cleardb.com;dbname=heroku_fee62f08e9a254b');
 define('DB_USER', 'b95d456714e818');
 define('DB_PASSWORD', 'e9ec1df8');

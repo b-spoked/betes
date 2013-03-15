ae2ae3d8af1d6d47795d64d61f825ce7e0e4c282
jhbate <jamie@almservices.co.nz>
2013-03-15 14:26:24 +1300

    config for heroku

--- web_app/api/config.php
+++ web_app/api/config.php
@@ -1,4 +1,11 @@
 <?php
-define('DB_SERVER', 'mysql://b95d456714e818:e9ec1df8@us-cdbr-east-03.cleardb.com;dbname=heroku_fee62f08e9a254b');
-define('DB_USER', 'b95d456714e818');
-define('DB_PASSWORD', 'e9ec1df8');
+$url=parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"],1);
+
+define('DB_SERVER', 'mysql:host='+$server+'dbname='+$db);
+define('DB_USER', $username);
+define('DB_PASSWORD', $password);
